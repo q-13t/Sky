17599d0750e5bed35f2194f72eb125c6a98e441e
q-13t <vova29092002@gmail.com>
2023-06-09 19:17:55 +0300

    IMP: SwipeRefreshLayout & minor starting

--- app/src/main/java/com/learning/sky/API/WeatherAPI.java
+++ app/src/main/java/com/learning/sky/API/WeatherAPI.java
@@ -1,6 +1,5 @@
+package com.learning.sky.API;
 
 public class WeatherAPI {
-    public static void main(String[] args) {
-        System.out.println("Hell");
-    }
+
 }
--- app/src/main/java/com/learning/sky/MainActivity.java
+++ app/src/main/java/com/learning/sky/MainActivity.java
@@ -1,8 +1,14 @@
 package com.learning.sky;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwipeRefreshLayout srl = findViewById(R.id.swipeRefreshLayout);
+        srl.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        Random random = new Random();
+                        LinearLayout parent = (LinearLayout) findViewById(R.id.hourlyForecast);
+                        if(parent.getChildCount()!=0){
+                            parent.removeAllViews();
+                        }
+                        for (int i = 1; i <= 24; i++) {
+                            View custom = getLayoutInflater().inflate(R.layout.forecasth_hourly, null);
+                            TextView tv1 = (TextView) custom.findViewById(R.id.rain_percentage);
+                            tv1.setText(random.nextInt(1000)+"");
+                            TextView tv2 = (TextView) custom.findViewById(R.id.time);
+                            tv2.setText("00:00");
+                            TextView tv3 = (TextView) custom.findViewById(R.id.temperature);
+                            tv3.setText(random.nextInt(1000)+"*");
+                            parent.addView(custom);
+                        }
+                        srl.setRefreshing(false);
+                    }
+                }
+        );
+    }
+    public void ButtonTest(View view){
+        LinearLayout parent = (LinearLayout) findViewById(R.id.hourlyForecast);
+        if(parent.getChildCount()!=0){
+            parent.removeAllViews();
+        }
+
+
+        for (int i = 1; i <= 24; i++) {
+            View custom = getLayoutInflater().inflate(R.layout.forecasth_hourly, null);
+            TextView tv1 = (TextView) custom.findViewById(R.id.rain_percentage);
+            tv1.setText(i+"");
+            TextView tv2 = (TextView) custom.findViewById(R.id.time);
+            tv2.setText("00:00");
+            TextView tv3 = (TextView) custom.findViewById(R.id.temperature);
+            tv3.setText(i+"*");
+            parent.addView(custom);
+        }
     }
 }
