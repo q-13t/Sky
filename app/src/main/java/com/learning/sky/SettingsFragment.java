72352c37c12e6b3a9c423eae5436e6091b570e4c
q-13t <vova29092002@gmail.com>
2023-06-22 17:55:57 +0300

    UPD: SettingsFragment.java -> Light/Dark modes

--- app/src/main/java/com/learning/sky/SettingsFragment.java
+++ app/src/main/java/com/learning/sky/SettingsFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatDelegate;
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
+import com.learning.sky.dao.ApplicationSettings;
+
+import java.util.Objects;
+
 public class SettingsFragment extends Fragment {
+    View fragment;
+
     public SettingsFragment() {
 
     }
@@ -20,24 +25,45 @@ public class SettingsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (fragment == null) {
+            fragment = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        SwitchCompat switchCompatSystemMode = view.findViewById(R.id.st_system_mode);
-        SwitchCompat switchCompatDarkMode = view.findViewById(R.id.st_dark_mode);
+            SwitchCompat switchCompatSystemMode = fragment.findViewById(R.id.st_system_mode);
+            SwitchCompat switchCompatDarkMode = fragment.findViewById(R.id.st_dark_mode);
+            MainActivity main = new MainActivity();
 
-        switchCompatSystemMode.setOnCheckedChangeListener((compoundButton, b) -> switchCompatDarkMode.setEnabled(switchCompatSystemMode.isChecked()));
 
-        switchCompatDarkMode.setOnCheckedChangeListener(((compoundButton, b) -> {
-            if (switchCompatDarkMode.isChecked()) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                Toast.makeText(view.getContext(), "Dark Mode Disabled", Toast.LENGTH_SHORT).show();
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                Toast.makeText(view.getContext(), "Dark Mode Enabled", Toast.LENGTH_SHORT).show();
+            if(Objects.equals(ApplicationSettings.getPreferenceValue(PreferenceType.BOOLEAN, getString(R.string.CUSTOM_MODE), requireActivity()), false)){
+                switchCompatSystemMode.setChecked(false);
+                switchCompatDarkMode.setEnabled(false);
+            }else{
+                switchCompatSystemMode.setChecked(true);
             }
-        }));
+            switchCompatDarkMode.setChecked(Objects.equals(ApplicationSettings.getPreferenceValue(PreferenceType.BOOLEAN, getString(R.string.DARK_MODE), requireActivity()), true));
 
-        return view;
+            switchCompatSystemMode.setOnCheckedChangeListener((compoundButton, b) -> {
+                if(switchCompatSystemMode.isChecked()){
+                    ApplicationSettings.setPreferenceValue(getString(R.string.CUSTOM_MODE),true, requireActivity());
+                    switchCompatDarkMode.setEnabled(true);
+                }else{
+                    ApplicationSettings.setPreferenceValue(getString(R.string.CUSTOM_MODE),false, requireActivity());
+                    switchCompatDarkMode.setEnabled(false);
+                }
+                main.repaint(requireActivity());
+            });
+
+            switchCompatDarkMode.setOnCheckedChangeListener(((compoundButton, b) -> {
+                if (switchCompatDarkMode.isChecked()) {
+                    ApplicationSettings.setPreferenceValue(getString(R.string.DARK_MODE),true, requireActivity());
+                } else {
+                    ApplicationSettings.setPreferenceValue(getString(R.string.DARK_MODE),false, requireActivity());
+                }
+               main.repaint(requireActivity());
+            }));
+        }
+
+
+        return fragment;
     }
 }
