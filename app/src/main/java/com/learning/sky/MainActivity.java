0a9e56983f3771746d0775550d70cd8c43e0fe33
q-13t <vova29092002@gmail.com>
2023-06-26 15:01:14 +0300

    UPD: APP lifecycle logic

--- app/src/main/java/com/learning/sky/MainActivity.java
+++ app/src/main/java/com/learning/sky/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.learning.sky.dao.ApplicationSettings;
 
@@ -16,7 +17,7 @@ import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+private static Fragment lastFragment;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		repaint(this);
@@ -31,17 +32,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	@Override
 	protected void onResume() {
 		super.onResume();
-		if (Objects.equals(ApplicationSettings.getPreferenceValue(PreferenceType.STRING, getString(R.string.LAST_PAGE), this), getString(R.string.DEFAULT))) {
-			ApplicationSettings.setPreferenceValue(getString(R.string.LAST_PAGE), getString(R.string.WEATHER), this);
+		if(lastFragment!=null){
+			getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, lastFragment).commit();
+		}else{
+			lastFragment = WeatherFragment.newInstance();
 			getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, WeatherFragment.newInstance()).commit();
-		} else {
-			if (Objects.equals(ApplicationSettings.getPreferenceValue(PreferenceType.STRING, getString(R.string.LAST_PAGE), this), R.string.WEATHER)) {
-				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, WeatherFragment.newInstance()).commit();
-			} else if (Objects.equals(ApplicationSettings.getPreferenceValue(PreferenceType.STRING, getString(R.string.LAST_PAGE), this), getString(R.string.SETTINGS))) {
-				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, SettingsFragment.newInstance()).commit();
-			}
 		}
-//        Objects.requireNonNull(getSupportActionBar()).hide();
 	}
 
 	@Override
@@ -53,17 +49,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	public void onClick(View view) {
 		switch (view.getId()) {
 			case R.id.settings: {
-				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, SettingsFragment.newInstance()).commit();
-				ApplicationSettings.setPreferenceValue(getString(R.string.LAST_PAGE), getString(R.string.SETTINGS), this);
+				lastFragment = SettingsFragment.newInstance();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, lastFragment).commit();
 				break;
 			}
 			case R.id.weather: {
-				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, WeatherFragment.newInstance()).commit();
-				ApplicationSettings.setPreferenceValue(getString(R.string.LAST_PAGE), getString(R.string.WEATHER), this);
+				lastFragment = WeatherFragment.newInstance();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, lastFragment).commit();
 				break;
 			}
 			case R.id.city_search: {
-				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, CitySearchFragment.newInstance()).commit();
+				lastFragment = CitySearchFragment.newInstance();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, lastFragment).commit();
 				break;
 			}
 		}
