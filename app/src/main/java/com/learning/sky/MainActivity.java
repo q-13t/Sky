920ddd6784680650176c772898fc7ea4b4af6051
q-13t <vova29092002@gmail.com>
2023-07-05 17:32:29 +0300

    UPD: UI UX

--- app/src/main/java/com/learning/sky/MainActivity.java
+++ app/src/main/java/com/learning/sky/MainActivity.java
@@ -3,8 +3,9 @@ package com.learning.sky;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -15,13 +16,14 @@ import com.learning.sky.FragmentController.WeatherFragment;
 import com.learning.sky.dao.ApplicationSettings;
 import com.learning.sky.dao.FileOperator;
 
+import java.lang.ref.WeakReference;
 import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-	public static Context main;
+	public WeakReference<Context> main;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +31,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		super.onCreate(savedInstanceState);
 //		supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		if (main == null) main = this;
+		// Read data from file
+//		Create needed views for side menu
+		LinearLayout sideNav = findViewById(R.id.brief_data_container);
+		for (int i = 0; i < 5; i++) {
+			getLayoutInflater().inflate(R.layout.brief_detail, sideNav, true);
+			LinearLayout childAt = (LinearLayout) sideNav.getChildAt(i);
+			childAt.setOnClickListener((View view) -> {
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, WeatherFragment.newInstance()).commit();
+				((DrawerLayout) findViewById(R.id.main_drawer)).close();
+			});
+			((TextView) childAt.getChildAt(0)).setText(getString(R.string.city_name_placeholder));
+
+		}
+
+
+		if (main == null) main = new WeakReference<>(this);
 		if (FileOperator.listFiles().length == 0) {
 			getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, CitySearchFragment.newInstance()).commit();
 		} else {
 //			TODO: set default city weather from Files
 			getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, WeatherFragment.newInstance()).commit();
-			WeatherFragment.FillDataFromFile((String) ApplicationSettings.getPreferenceValue(PreferenceType.STRING,"DEFAULT_LOCATION",this));
+			WeatherFragment.FillDataFromFile((String) ApplicationSettings.getPreferenceValue(PreferenceType.STRING, "DEFAULT_LOCATION", this));
 		}
 
 //		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
